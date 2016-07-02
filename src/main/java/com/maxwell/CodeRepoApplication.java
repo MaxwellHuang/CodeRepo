70db08290116aeffd654a2d7ad4d638ed32428c8
maxwell <hxyxm2008@qq.com>
2016-07-02 21:57:23 +0800

    Initial commit

---
+++ src/main/java/com/maxwell/CodeRepoApplication.java
@@ -0,0 +1,12 @@
+package com.maxwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeRepoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maxwell/CodeRepoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maxwell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CodeRepoApplication.class)
+public class CodeRepoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
