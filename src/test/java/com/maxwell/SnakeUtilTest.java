78d1ed500cdcacc537372d6e73a9226749ed1281
Maxwell <huangxiongyi@gengee.cn>
2016-07-03 17:51:45 +0800

    format console

--- src/test/java/com/maxwell/SnakeUtilTest.java
+++ src/test/java/com/maxwell/SnakeUtilTest.java
@@ -13,14 +13,14 @@ public class SnakeUtilTest {
     @Test
     public void test1() throws Exception {
         SnakeUtil snakeUtil = new SnakeUtil();
-        List<Integer> teams = snakeUtil.createTeams(15);
+        List<Integer> teams = snakeUtil.createTeams(6);
         printLists(teams);
     }
 
     @Test
     public void test2() throws Exception {
         SnakeUtil snakeUtil = new SnakeUtil();
-        printSchedule(snakeUtil.createTeams(15));
+        printSchedule(snakeUtil.createTeams(6));
 
     }
 
@@ -30,12 +30,12 @@ public class SnakeUtilTest {
         rightTeams.sort(Comparator.comparing(Integer::intValue).reversed());
         SnakeUtil snakeUtil = new SnakeUtil();
         for (int i = 1; i < teams.size(); i++) {
+            System.out.println("第" + i + "轮:");
             for (int j = 0; j < leftTeams.size(); j++) {
                 Integer right = rightTeams.get(j);
                 Integer left = leftTeams.get(j);
-                System.out.println((left == 0 ? "轮空" : left) + "-" + (right == 0 ? "轮空" : right));
+                System.out.println((String.format("%2s%1s%2s", (left == 0 ? "●" : left), "-", (right == 0 ? "●" : right))));
             }
-            System.out.println("----");
             snakeUtil.moveListElement(leftTeams, rightTeams);
         }
     }
@@ -45,16 +45,17 @@ public class SnakeUtilTest {
         LinkedList<Integer> leftTeams = new LinkedList<>(teams.subList(0, teams.size() / 2));
         LinkedList<Integer> rightTeams = new LinkedList<>(teams.subList(teams.size() / 2, teams.size()));
         Collections.reverse(rightTeams);
-        System.out.printf(String.format("%3.8s", ""));
+        System.out.printf(String.format("%6s", ""));
         Map<Integer, List<String>> teamMap = initMap(teams.size());
         for (int i = 1; i < teams.size(); i++) {
+            System.out.printf(String.format("%6s", i));
             for (int j = 0; j < leftTeams.size(); j++) {
                 Integer right = rightTeams.get(j);
                 Integer left = leftTeams.get(j);
                 if (right != 0)
-                    teamMap.get(right).add(left == 0 ? "空" : left.toString());
+                    teamMap.get(right).add(left == 0 ? "●" : left.toString());
                 if (left != 0)
-                    teamMap.get(left).add(right == 0 ? "空" : right.toString());
+                    teamMap.get(left).add(right == 0 ? "●" : right.toString());
             }
             snakeUtil.moveListElement(leftTeams, rightTeams);
         }
